429f84ba8910219c436405fe59d1d71bbdd9b193
Enclosed Letter <C.LuisMartinez.A@gmail.com>
2020-04-10 15:26:46 -0700

    met MVP

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+  <h1 class="welcomeMessage">Welcome to my dummy component!</h1>
+  )
+}
+
+export default Welcome;
---
+++ client/src/components/Welcome.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect'
+import Welcome from "./Welcome";
+
+test("Welcome message is there", () => {
+   //Arrange 
+    const { getByText } = render(<Welcome />);
+  // Act
+  const welcome = getByText(/Welcome to my dummy component!/i);
+  // Assert
+  expect(welcome).toBeInTheDocument();
+});
+
+
+//  test("renders greeting on Greeting component", () => {
+//     //Arrange 
+//      const { getByText } = render(<Welcome />);
+//    // Act
+//    const welcome = getByText(/Welcome to my dummy component!/i);
+//    // Assert
+//    expect(welcome).toBeInTheDocument();
+//  });
+ 
